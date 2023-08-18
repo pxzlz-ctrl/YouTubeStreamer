9de182fa0db896653a18cf267f2d7f5762064e7a
Pxzlz <91290196+pxzlz-ctrl@users.noreply.github.com>
2023-08-18 18:05:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
